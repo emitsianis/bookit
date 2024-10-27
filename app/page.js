97925a8c975628b714093964a7c625189ec4437d
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-10-27 11:52:02 +0200

    Add: Room card

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
+import rooms from '@/data/rooms.json';
+import RoomCard from '@/app/components/RoomCard';
+
 export default function Home() {
   return (
     <>
-      <h1>Bookit App</h1>
+      {
+        rooms?.length
+          ? (
+            rooms.map((room) => <RoomCard key={room.id} room={room} />)
+          )
+          : (
+            <p>No rooms available at the moment</p>
+          )
+      }
     </>
   );
 }
